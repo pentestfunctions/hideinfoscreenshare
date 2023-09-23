4e80afe049e0727cefc6f32e4cab676dedc4d974
pentestfunctions <144001335+pentestfunctions@users.noreply.github.com>
2023-09-24 05:22:13 +1300

    Update hideinfo.js

--- hideinfo.js
+++ hideinfo.js
@@ -6,59 +6,95 @@
 // @author       You
 // @match        *://*/*
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
+
 
 (function() {
     'use strict';
 
+    // Create an overlay element
+    const overlay = document.createElement('div');
+    overlay.style.position = 'fixed';
+    overlay.style.top = '0';
+    overlay.style.left = '0';
+    overlay.style.width = '100vw';
+    overlay.style.height = '100vh';
+    overlay.style.backgroundColor = 'gray'; // Non-transparent gray
+    overlay.style.zIndex = '1000000'; // High z-index to cover everything
+    overlay.innerHTML = '<div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center;">' +
+                        '<div class="spinner" style="margin: 20px auto; width: 40px; height: 40px; position: relative;">' +
+                        '<div style="box-sizing: border-box; display: block; position: absolute; width: 32px; height: 32px; margin: 8px; border: 8px solid #fff; border-radius: 50%; animation: spinner 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite; border-color: #000 transparent transparent transparent;"></div>' +
+                        '</div>' +
+                        '<p style="color: #fff; font-size: 18px;">Redacting...</p>' +
+                        '</div>';
+    document.body.appendChild(overlay); // Append overlay immediately when the script runs
+
+    // Define the spinner animation
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '@keyframes spinner {0% { transform: rotate(0deg); } 100% { transform: rotate(360deg); }}';
+    document.getElementsByTagName('head')[0].appendChild(style);
+
     // Define the sensitive information to redact
     const toRedact = {
-        phoneNumbers: [
-            '123-456-7890',
-            '987-654-3210'
-            // Add more phone numbers as needed
-        ],
-        ipAddresses: [
-            '192.168.1.1',
-            '10.0.0.1'
-            // Add more IP addresses as needed
-        ],
-        emailAddresses: [
-            'example@example.com',
-            'test@test.com'
-            // Add more email addresses as needed
-        ]
+        phoneNumbers: ['REDACTED@phone.com', 'REDACTED@phone.com'],
+        ipAddresses: ['REDACTED@ip.com', 'REDACTED@ip.com'],
+        emailAddresses: ['REDACTED@admin.com', 'REDACTED@admin.com'],
+        usernames: ['REDACTED_USER', 'REDACTED_USER'],
+        names: ['REDACTED_NAME', 'REDACTED_NAME', 'REDACTED_NAME', 'REDACTED_NAME']
     };
 
     // Define the redacted strings
     const redacted = {
-        phoneNumber: 'REDACTED@phone.com',
-        ipAddress: 'REDACTED@ip.com',
-        emailAddress: 'REDACTED@admin.com'
+        phoneNumbers: 'REDACTED@phone.com',
+        ipAddresses: 'REDACTED@ip.com',
+        emailAddresses: 'REDACTED@admin.com',
+        usernames: 'REDACTED_USER',
+        names: 'REDACTED_NAME'
     };
 
-    // Replace the sensitive information with the redacted strings
-    function redactInformation() {
-        let bodyText = document.body.innerHTML;
-
-        toRedact.phoneNumbers.forEach((phoneNumber) => {
-            const regex = new RegExp(phoneNumber, 'g');
-            bodyText = bodyText.replace(regex, redacted.phoneNumber);
+    function redactText(text) {
+        let redactedText = text;
+        Object.keys(toRedact).forEach(category => {
+            toRedact[category].forEach(item => {
+                const regex = new RegExp(item, 'gi'); // Case insensitive matching
+                redactedText = redactedText.replace(regex, redacted[category]);
+            });
         });
-
-        toRedact.ipAddresses.forEach((ipAddress) => {
-            const regex = new RegExp(ipAddress, 'g');
-            bodyText = bodyText.replace(regex, redacted.ipAddress);
-        });
-
-        toRedact.emailAddresses.forEach((emailAddress) => {
-            const regex = new RegExp(emailAddress, 'g');
-            bodyText = bodyText.replace(regex, redacted.emailAddress);
-        });
-
-        document.body.innerHTML = bodyText;
+        return redactedText;
     }
 
-    // Run the redactInformation function when the webpage has finished loading
-    window.addEventListener('load', redactInformation);
+    function redactNode(node) {
+        // Preserve user selection
+        const selection = window.getSelection();
+        const range = selection.rangeCount > 0 ? selection.getRangeAt(0) : null;
+
+        if (node.nodeType === Node.TEXT_NODE) {
+            node.nodeValue = redactText(node.nodeValue);
+        } else if (node.nodeType === Node.ELEMENT_NODE) {
+            if (node.tagName === 'INPUT' || node.tagName === 'TEXTAREA') {
+                node.value = redactText(node.value);
+            }
+            for (const child of node.childNodes) {
+                redactNode(child);
+            }
+        }
+
+        // Restore user selection
+        if (range) {
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
+
+    // Run the redactNode function when the webpage has finished loading
+    window.addEventListener('load', () => {
+        redactNode(document.body);
+        document.body.removeChild(overlay); // Remove overlay after initial redaction
+
+        // Watch for changes in the DOM and apply redaction as necessary
+        new MutationObserver(() => redactNode(document.body))
+            .observe(document.body, {childList: true, subtree: true});
+    });
 })();
